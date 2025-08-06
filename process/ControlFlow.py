eeba7c82c59d12444d4490a84f36b90e58f03c5d
Saida Yengui <saida.yengui@gmail.com>
2025-08-06 07:10:19 +0100

    Check if Number is Positive, Negative, or Zero

--- process/ControlFlow.py
+++ process/ControlFlow.py
@@ -1,3 +1,11 @@
 for i in range(1, 21):
     if i % 2 == 0:
         print(i)
+number = -5
+
+if number > 0:
+    print("Number "+ str(number) + " Positive")
+elif number < 0:
+    print("Number"+ str(number) + " Negative")
+else:
+    print("Number"+ str(number)+ " Zero")
