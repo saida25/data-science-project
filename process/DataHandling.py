c9aeb466c21e38b8d3fc003c84559b9445d69ec3
Saida Yengui <saida.yengui@gmail.com>
2025-08-09 16:48:19 +0100

    Cleaning Data

--- process/DataHandling.py
+++ process/DataHandling.py
@@ -16,4 +16,22 @@ df = pd.read_csv("/home/saida/IdeaProjects/data-science-project/data/titanic.csv
 #print(df.info())
 
 # Summary statistics
-print(df.describe())
+#print(df.describe())
+
+# Count missing values
+print(df.isnull().sum())
+
+# Fill missing Age with mean
+df['age'].fillna(df['age'].mean(), inplace=True)
+ 
+print("Missing values after filling Age:", df.isnull().sum())
+
+# Drop rows where 'embarked' is missing
+df.dropna(subset=['embarked'], inplace=True)
+
+# Remove duplicates
+df.drop_duplicates(inplace=True)
+
+print("Shape after cleaning:", df.shape)
+# Save cleaned data to a new CSV file   
+df.to_csv("/home/saida/IdeaProjects/data-science-project/data/titanic_cleaned.csv", index=False)        
