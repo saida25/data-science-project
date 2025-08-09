af3aa8d1e772ca71f5db3f692d10b43ea1fc3dc5
Saida Yengui <saida.yengui@gmail.com>
2025-08-09 17:09:18 +0100

    Selecting & Filtering

--- process/DataHandling.py
+++ process/DataHandling.py
@@ -19,37 +19,33 @@ df = pd.read_csv("/home/saida/IdeaProjects/data-science-project/data/titanic.csv
 #print(df.describe())
 
 # Count missing values
-print(df.isnull().sum())
+#print(df.isnull().sum())
 
 # Fill missing Age with mean
-df['age'].fillna(df['age'].mean(), inplace=True)
+#df['age'].fillna(df['age'].mean(), inplace=True)
  
-print("Missing values after filling Age:", df.isnull().sum())
+#print("Missing values after filling Age:", df.isnull().sum())
 
 # Drop rows where 'embarked' is missing
-df.dropna(subset=['embarked'], inplace=True)
+#df.dropna(subset=['embarked'], inplace=True)
 
 # Remove duplicates
-df.drop_duplicates(inplace=True)
-
-print("Shape after cleaning:", df.shape)
-# Save cleaned data to a new CSV file   
-df.to_csv("/home/saida/IdeaProjects/data-science-project/data/titanic_cleaned.csv", index=False)        
+#df.drop_duplicates(inplace=True)
+  
 # Select a column
-ages = df['Age']
+ages = df['age']
 print(ages.head())
 
 # Select multiple columns
-subset = df[['Name', 'Age', 'Sex']]
+subset = df[['sex','age', 'survived']]
 print(subset.head())
 
 # Filter: Passengers older than 30
-older_than_30 = df[df['Age'] > 30]
+older_than_30 = df[df['age'] > 30]
 print(older_than_30.shape)
 
-# Filter: Female passengers who survived
-female_survivors = df[(df['Sex'] == 'female') & (df['Survived'] == 1)]
-print(female_survivors.shape)
-# Group by 'Pclass' and calculate average age
-average_age_by_class = df.groupby('Pclass')['Age'].mean()
-print(average_age_by_class) 
+
+
+print("Shape after cleaning:", df.shape)
+# Save cleaned data to a new CSV file   
+df.to_csv("/home/saida/IdeaProjects/data-science-project/data/titanic_cleaned.csv", index=False)      
